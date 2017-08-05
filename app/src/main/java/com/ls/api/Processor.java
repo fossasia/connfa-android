90b8e992bc3169262dead73c34cc19afae9566c4
heysadboy <heysadboy@gmail.com>
2017-08-05 13:41:25 +0900

    Making the chages to read locations from the API

--- app/src/main/java/com/ls/api/Processor.java
+++ app/src/main/java/com/ls/api/Processor.java
@@ -1,5 +1,8 @@
 package com.ls.api;
 
+import android.util.Log;
+
+import com.ls.drupalcon.model.data.Location;
 import com.ls.drupalcon.model.data.Track;
 
 import org.json.JSONArray;
@@ -17,22 +20,50 @@ public class Processor {
     private String output;
 
     public List<Track> listTrack = new ArrayList<>();
+    public List<Location> listLocation = new ArrayList<>();
 
 
     public Processor(String output) {
         this.output = output;
     }
 
-    public List<Track> trackProcessor()
-    {
+    public List<Location> locationProcessor() {
+
+        try {
+            JSONArray locations = new JSONArray(output);
+            int i;
+
+            Location location;
+
+            for (i = 0; i < locations.length(); i++) {
+                JSONObject locationJSONObject = locations.getJSONObject(i);
+
+                location = new Location();
+                location.setId(locationJSONObject.getLong("id"));
+                location.setName(locationJSONObject.getString("name"));
+                location.setAddress("Floor " + locationJSONObject.getJSONObject("floor") + ", " + locationJSONObject.getString("name"));
+                location.setLat(locationJSONObject.getDouble("latitude"));
+                location.setLon(locationJSONObject.getDouble("longitude"));
+
+                Log.e("name:",locationJSONObject.getString("name"));
+
+                listLocation.add(location);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return listLocation;
+    }
+
+    public List<Track> trackProcessor() {
         try {
             JSONArray tracks = new JSONArray(output);
-            int i, j;
+            int i;
 
             Track track;
 
-            for(i=0;i<tracks.length();i++)
-            {
+            for (i = 0; i < tracks.length(); i++) {
                 JSONObject tracksJSONObject = tracks.getJSONObject(i);
 
                 track = new Track();
@@ -42,7 +73,7 @@ public class Processor {
                 listTrack.add(track);
             }
 
-        }catch (JSONException e) {
+        } catch (JSONException e) {
             e.printStackTrace();
         }
         return listTrack;
