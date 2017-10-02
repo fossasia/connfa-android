8f763451b4310405a339d31e27e3a211f4a32088
Varun Kumar <heysadboy@gmail.com>
2017-10-03 00:53:06 +0530

    Adding support to read sessions (#15)
    * Changing processor to read dates
    
    * Adding support to read sessions
    
    Solving issue https://github.com/fossasia/connfa-android/issues/11
    
    Able to see tabs for days now but Drupal functioanlity creating problem to modify the app to populate adapter and use DAO.

--- app/src/main/java/com/ls/api/Processor.java
+++ app/src/main/java/com/ls/api/Processor.java
@@ -2,6 +2,7 @@ package com.ls.api;
 
 import android.util.Log;
 
+import com.ls.drupalcon.model.data.Event;
 import com.ls.drupalcon.model.data.Location;
 import com.ls.drupalcon.model.data.Speaker;
 import com.ls.drupalcon.model.data.Track;
@@ -10,22 +11,105 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
+import java.util.Set;
 
 /**
  * Created by Varun Kumar on 7/27/2017.
  */
 
 public class Processor {
-    public List<Track> trackList = new ArrayList<>();
-    public List<Location> locationList = new ArrayList<>();
-    public List<Speaker> speakerList = new ArrayList<>();
+    private List<Track> trackList = new ArrayList<>();
+    private List<Location> locationList = new ArrayList<>();
+    private List<Speaker> speakerList = new ArrayList<>();
+    private List<Event> eventList = new ArrayList<>();
+    private List<Long> dayList = new ArrayList<>();
+    private List<String> days = new ArrayList<>();
     private String output;
 
 
     public Processor(String output) {
         this.output = output;
+    }
+
+    public List<String> getDays() {
+        return days;
+    }
+
+    public List<Long> getDayList() {
+        return dayList;
+    }
+
+    public List<Event> eventProcessor() {
+
+        try {
+            JSONArray events = new JSONArray(output);
+            int i;
+
+            Event event;
+
+            for (i = 0; i < events.length(); i++) {
+                JSONObject eventJSONObject = events.getJSONObject(i);
+                event = new Event();
+
+                event.setId(eventJSONObject.getLong("id"));
+                event.setFromTime(eventJSONObject.getString("starts-at"));
+                event.setToTime(eventJSONObject.getString("ends-at"));
+
+                String startT = event.getFromTime().substring(0, event.getFromTime().indexOf("T"));
+                String endT = event.getToTime().substring(0, event.getToTime().indexOf("T"));
+
+                days.add(startT);
+                days.add(endT);
+                dayList.add(convertTime(startT));
+                dayList.add(convertTime(endT));
+
+                JSONObject typeJSONObject = eventJSONObject.getJSONObject("session-type");
+                event.setType(typeJSONObject.getLong("id"));
+
+                event.setName(eventJSONObject.getString("title"));
+
+                List<Long> speakers = new ArrayList<>();
+                int j;
+
+                JSONArray speakersArray = eventJSONObject.getJSONArray("speakers");
+
+                for (j = 0; j < speakersArray.length(); j++) {
+                    JSONObject speaker = speakersArray.getJSONObject(j);
+                    speakers.add(speaker.getLong("id"));
+                }
+
+                event.setSpeakers(speakers);
+
+                JSONObject trackObject = eventJSONObject.getJSONObject("track");
+                event.setTrack(trackObject.getLong("id"));
+
+                event.setDescription(eventJSONObject.getString("long-abstract"));
+                event.setLink("signup-url");
+
+                eventList.add(event);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        Set uniqueValues = new HashSet(dayList);
+        dayList.clear();
+        dayList.addAll(uniqueValues);
+
+        Set uniqueValues2 = new HashSet(days);
+        days.clear();
+        days.addAll(uniqueValues2);
+
+        return eventList;
     }
 
     public List<Speaker> speakerProcessor() {
@@ -134,6 +218,21 @@ public class Processor {
             e.printStackTrace();
         }
         return trackList;
+    }
+
+    private static long convertTime(String time) {
+        if (time == null) {
+            return 0;
+        }
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        try {
+            Date date = format.parse(time);
+            return date.getTime();
+
+        } catch (ParseException e) {
+            return 0;
+        }
     }
 
 }
